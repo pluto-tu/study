640c8be39ec8c5202c2299c2d9580446b3e90652
tubangwu <tubangwu@peken.com>
2019-03-08 09:40:09 +0800

    add

--- weixin-test-web/src/main/java/leecode/SortStudy.java
+++ weixin-test-web/src/main/java/leecode/SortStudy.java
@@ -150,7 +150,7 @@ public class SortStudy<T extends Comparable<T>> extends Sort<T> {
     public static void main(String[] args) {
         SortStudy sortStudy = new SortStudy();
         Integer[] nums = {3, 1, 6, 2, 5, 8, 4, 7};
-        sortStudy.quickSortTest(nums,0,nums.length-1);
+        sortStudy.qucikSort(nums,0,nums.length-1);
 
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + "--");
